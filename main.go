9911a324b6dbaf51d55b95390776af25f3b6acc2
totalmainia <116669562+totalmainia@users.noreply.github.com>
2025-11-17 19:00:26 -0600

    Update main.go

--- main.go
+++ main.go
@@ -43,7 +43,7 @@ func main() {
 		log.Fatal("Cannot connect to database:", err)
 	}
 	log.Println("Connected to PostgreSQL!")
-
+	log.Println("William Ellithorpe")
 	http.HandleFunc("/users", getUsers)
 	http.HandleFunc("/add", addUser)
 
